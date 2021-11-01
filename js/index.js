b3653792720dd2d4ad5ddc250179066940556bb3
Julio Fuentes <juliodddddd1234@gmail.com>
2021-11-01 15:09:20 -0600

    Snake game is ready

---
+++ js/index.js
@@ -0,0 +1,48 @@
+import { update as updateSnake, draw as drawSnake,SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard =  document.getElementById('gameboard')
+
+function main(currentTime){
+    if (gameOver) {
+        if (confirm('Yoy lost. Press ok to restart')){
+            window.location = '/'
+        }
+        return
+    }
+
+
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    
+    //console.log('Render')
+    lastRenderTime = currentTime
+
+    update()//Update all the logic of the game
+    draw()//Draw everything on the screen based on the update loop 
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead())  || snakeIntersection()
+}
